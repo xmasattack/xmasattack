7897973a66e6b73fbdbe45304b706e63796b9a4e
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-19 17:39:56 -0500

    Base of the token

---
+++ contracts/NFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+/* Total Supply = 100%
+    20 million burnt (20%) to larget state
+    25 million reward pool (25%)
+    25 million public sale(25%)
+    5 million marketing wallet(5%)
+    5 million RnD spending(5%)
+    Transaction tax:
+    2% tax gows back to reward pool
+    2% tax goes back to marketing wallet
+*/
