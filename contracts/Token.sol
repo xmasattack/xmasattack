a05a5d811e44ae9a340f956d84b2dd0290d3939c
RafaCypherpunk <rafafuentesrangel@gmail.com>
2021-09-20 23:24:01 -0500

    Token 1

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,4 +12,17 @@
 */
 
 // Token BEP-20 
+pragma solidity >=0.6.0 <0.9.0;
+
+abstract contract Context {
+    function _msgSender() internal view returns (address payable) {
+        return payable(msg.sender);
+    }
+
+    function _msgData() internal view returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
 
