96be2b43cba40c2e8b413d7ef9de928ceafb15d0
RafaCypherpunk <rafafuentesrangel@gmail.com>
2021-09-20 23:25:35 -0500

    Continue of library

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -47,5 +47,31 @@ interface IERC20 {
   */
   function name() external view returns (string memory);
 
+  /**
+   * @dev Returns the bep token owner.
+   */
+  function getOwner() external view returns (address);
+
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
+
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Returns the remaining number of tokens that `spender` will be
+   * allowed to spend on behalf of `owner` through {transferFrom}. This is
+   * zero by default.
+   *
+   * This value changes when {approve} or {transferFrom} are called.
+   */
 
 
