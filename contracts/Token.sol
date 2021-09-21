da86e0f333b5836848bb6515a3ba51e3912171b1
RafaCypherpunk <rafafuentesrangel@gmail.com>
2021-09-20 23:25:06 -0500

    Library

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -26,3 +26,26 @@ abstract contract Context {
 }
 
 
+interface IERC20 {
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
+
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external view returns (uint8);
+
+  /**
+   * @dev Returns the token symbol.
+   */
+  function symbol() external view returns (string memory);
+
+  /**
+  * @dev Returns the token name.
+  */
+  function name() external view returns (string memory);
+
+
+
